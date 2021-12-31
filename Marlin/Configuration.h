bfc70963c0644b4eacbde2e479c3aa9dc66ef44d
Przemek Grondek <przemek@grondek.pl>
2021-12-31 13:49:52 +0100

    Change default max feed rate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -907,7 +907,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 7, 50 }
+#define DEFAULT_MAX_FEEDRATE          { 225, 225, 3, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
