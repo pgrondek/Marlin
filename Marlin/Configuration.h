7d148f699d5e47167b61237882d948ac4f58f232
Przemek Grondek <przemek@grondek.pl>
2022-01-03 23:49:17 +0100

    Set machine UUID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -147,7 +147,7 @@
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "8b36bfa6-3847-407f-a6d7-9dd3ea8f837f"
 
 /**
  * Define the number of coordinated linear axes.
